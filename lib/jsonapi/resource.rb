b6c9d3a74f4837174f63dab27aa3c98bf715b84d
Nathan Palmer <nathan@nathanpalmer.com>
2015-10-14 10:59:38 -0400

    Allow explicitly setting the type on a resource

--- lib/jsonapi/resource.rb
+++ lib/jsonapi/resource.rb
@@ -578,6 +578,10 @@ module JSONAPI
         _model_hints[model.to_s.gsub('::', '/').underscore] = resource_type.to_s
       end
 
+      def model_type(type)
+        @_type = type.to_sym
+      end
+
       def filters(*attrs)
         @_allowed_filters.merge!(attrs.inject({}) { |h, attr| h[attr] = {}; h })
       end
